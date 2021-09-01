9a08a1b51072a66e0a4dda9734b606e35baf1dea
IRONALLEN <87197368+IRONALLEN@users.noreply.github.com>
2021-09-01 19:10:09 -0400

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,38 @@
+#task 1
+print("Hello World")
+#task 2
+v = "Jean"
+print("Hello", v )
+#task 3
+n = "jean"
+print("Hello "+v)
+#task 4
+a = 21
+print("Hello", a)
+#task 5
+x = 33
+print("Hello "+str(x))
+
+#Bonus
+#task 1
+y = 44
+print("Hello "+str(y)) 
+z = "44"
+print("Hello "+z)
+#task 2
+food_1 = "Chicken"
+food_2 = "Burgers"
+print("I love to eat " +(food_1)+" and "+food_2)
+#task 3
+resturant = "bbq Ribs"
+fast_food = "Bone in Ribeyes"
+print(f"I love to eat {resturant}  and  {fast_food}")
+#task 4 
+name = input("What is your name?:")
+age = input("How old are you?:")
+food = input("What is your favorite food?:")
+drink = input("What is your favorite drink?:")
+print("Hello my name is "+name+" I am "+age+" I love to eat "+food+" my favorite is"+drink)
+
+
+
